df8e22b5400b0e6ca0c3cbc3bd89c68127b84465
Danila Bernat <danila6545@gmail.com>
2022-07-13 22:13:05 +0300

    init commit

---
+++ Weather Now/Weather Now/Weather_NowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_NowApp.swift
+//  Weather Now
+//
+//  Created by Бернат Данила on 13.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Weather_NowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ForecastView()
+        }
+    }
+}
