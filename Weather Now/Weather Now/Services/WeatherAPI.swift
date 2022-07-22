cca0520dc90f24f7908eb8e0766a84d78869e868
Danila Bernat <danila6545@gmail.com>
2022-07-22 13:30:57 +0300

    ref: put NetworkManager into file

--- Weather Now/Weather Now/Services/WeatherAPI.swift
+++ Weather Now/Weather Now/Services/WeatherAPI.swift
@@ -17,26 +17,3 @@ final class WeatherAPI {
         await WeatherAPI.networkManager.fetchWeather(latitude: lat, longitude: lon, requestType: .weather) as? CurrentWeatherData
     }
 }
-struct NetworkManager {
-    enum RequestType: String {
-        case forecast
-        case weather
-    }
-    func fetchWeather(latitude lat: Double, longitude lon: Double, requestType: RequestType) async -> Any? {
-        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/\(requestType.rawValue)?lat=\(lat)&lon=\(lon)&appid=\(Constants.apiKey)") else { return nil }
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            switch requestType {
-            case .forecast:
-                if let decodedResponse = try? JSONDecoder().decode(ForecastWeatherData.self, from: data) {
-                    return decodedResponse
-                }
-            case .weather:
-                if let decodedResponse = try? JSONDecoder().decode(CurrentWeatherData.self, from: data) {
-                    return decodedResponse
-                }
-            }
-        } catch { }
-        return nil
-    }
-}
